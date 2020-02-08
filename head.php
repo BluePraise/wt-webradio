ebc4b75f24689ee0cdf4cadd1ce4fc639c0c87ea
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-08 12:32:37 +0100

    actual php files

---
+++ head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>An Phantom Voices</title>
+    <meta name="author" content="Werkplaats Typgrafie Arnhem">
+    <link href="https://fonts.googleapis.com/css?family=Caudex:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+
+    <script src="https://cdn.jsdelivr.net/npm/p5@0.10.2/lib/p5.js"></script>
+    <script src="sketch.js"></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="mast-head">
+            <nav class="main-nav">
+                <ul>
+                    <li><a class="main-title" href="/">An Phantom Voices</a></li>
+                    <li><a href="/">December</a></li>
+                    <li><a href="/">January</a></li>
+                    <li><a href="/">February</a></li>
+                    <li><a href="/">March</a></li>
+                    <li><a href="/">April</a></li>
+                    <li><a href="/">May</a></li>
+                    <li><a href="/">June</a></li>
+                </ul>
+            </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include "head.php"; ?>
+
+<main id="content--container">
+	<div class="counter"></div>
+</main>
+
+<?php include "foot.php"; ?>
