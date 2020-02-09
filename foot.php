7c37b39ae7f754b74f86e621b1dbfeb37aba0a8e
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-09 10:46:04 +0100

    added conditional statement to footer

--- foot.php
+++ foot.php
@@ -1,9 +1,19 @@
+</div> <!-- .end of wrapper -->
 
-    </div> <!-- .end of wrapper -->
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<?php
+    // conditional statement to check if this is the index php so we can output the p5 js
+    $current_file_name = basename($_SERVER['PHP_SELF']);
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="js/p5/index.js"></script>
-    <script src="script.js"></script>
+    if ($current_file_name === "index.php") { ?>
+        <script src="js/p5/index.js"></script>
+        <script src="script.js"></script>
+
+    <?php } else { ?>
+
+
+    <?php } ?>
+
 </body>
 
 </html>
