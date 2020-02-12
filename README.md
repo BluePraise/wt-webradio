e1b6cbf651398f03e0ab19d423c7a202c1b4f126
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-12 12:00:31 +0100

    footer added and bones for all the other pages have been set

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ A project for Werkplaats Typografie in Arnhem.
 I've decided to build this project in PHP because it would be easier given the menu structure.
 If you like to change the session date, for now, you can go to script.js and change the countdown date in the variable.
 
+### week of Feb 10 2020
+
+I've decided to make everything as static as possible. As this is a temporarily maintained project. No need to put too much fancy php work in it. Tabea Nixdorff and Hannah Sakai shall provide me with content and I shall implement it throughout the site.
+
+### Structure of this site
+There's a page per month. A month needs to have an active class in the corresponding month and a p5 js file. The content container holds a left and a right side.
+
 ## Sources
 The base template for this is based off my [blank static template](https://github.com/BluePraise/blank-static-html)
 
 We've used [P5.js](https://p5js.org/) from [Lauren Lee McCarthy](https://lauren-mccarthy.com/Info)
 
 
-## Questions:
-
-- What is the minimum height of a content container?
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
     // Set the date we're counting down to
     var countDownDate = new Date("Mar 02, 2020 12:00").getTime();
@@ -6,26 +6,35 @@ $( document ).ready(function() {
     // Update the count down every 1 second
     var x = setInterval(function() {
 
-          // Get today's date and time
-          var now = new Date().getTime();
+        // Get today's date and time
+        var now = new Date().getTime();
 
-          // Find the distance between now and the count down date
-          var distance = countDownDate - now;
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
-          // Time calculations for days, hours, minutes and seconds
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-          // Display the result in the element with id="demo"
-          $(".counter").text("NEXT SESSION IN " + days + " DAYS~" + hours + " HOURS~" + minutes + " MINUTES ");
+        // Display the result in the element with id="demo"
+        $(".counter").text("NEXT SESSION IN " + days + " DAYS~" + hours + " HOURS~" + minutes + " MINUTES ");
 
-          // If the count down is finished, write some text
-          if (distance < 0) {
+        // If the count down is finished, write some text
+        if (distance < 0) {
             clearInterval(x);
             $(".counter").innerHTML = "EXPIRED";
-          }
+        }
     }, 1000);
 
+    // ACCORDION
+    $('.accordion--toggle').on('click', function() {
+        $(this).toggleClass('active');
+        $(this).next().toggle();
+
+        // find the List Item that's been activated
+        li = $(this).closest('li');
+        li.toggleClass('grow');
+    });
 
 })
