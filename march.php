f694e50fa909bd766686ac96a209e6bd5c469610
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-03-12 21:16:36 +0100

    switched march for feb

--- march.php
+++ march.php
@@ -22,34 +22,22 @@
 
         <div class="content--left">
             <div class="entry-a panel show">
-                <p>This month’s reading: “The Gender of Sound” by Anne Carson.</p>
-                <p>Additional texts in the reader: “Cry, Scream, Shout, Sing” from “Lexicon of the Mouth” by Brandon LaBelle, “When Our Lips Speak Together” by Luce Irigaray, “The Laugh of the Medusa” by Hélène Cixous, “A Phenomenology of Voice” from “Listening and Voice: Phenomenologies of Sound” by Don Ihde, “The Sounds of Feminist Theory” by Ruth Salvaggio, “Deep Listening” from “Deep Listening: A Composer’s Sound Practice” by Pauline Oliveros</p>
+
             </div>
             <div class="entry-b panel hide">
-                <p>This month, we followed the Sonic Meditation “Ear Ly / Sex Change” until the sound of the sirens went off.</p>
-                <p>Sonic Meditation meetings are inspired by Pauline Oliveros, who introduced them as non-verbal group work, as a way of attuning yourself to others, through one's own voice as well as exercising to listen to each other without the pressure of daily speech, often being marked by gendered parameters</p>
+
             </div>
             <div class="entry-c panel hide">
-                <p>Straw Phonation and Collective Gargle led by the WTFPC, followed by a Silent Lunch.
-                Monthly supper, along with other WTFPC activities, can be tracked here.</p>
+
             </div>
             <div class="entry-d panel hide">
-                <p>(engraved mirrors, performed with torch)</p>
-                <p>“Reflections on her voice” shares a selection of images, engraved into mirrors, chosen while looking at the presence of female voices in early Japanese theatre. Actresses were forbidden from the stage; all roles were played by men. Thus, femininity being marked by makeup, wigs, costumes and gestures. Visual representation compensated the absence of her voice.  The mirrors host a reflection of her voice into current space (music by Koppi Mizrahi).</p>
+
             </div>
 
         </div>
         <div class="content--right">
-            <ul class="audio-menu">
-                <li>
-                    <a class="entry-a">The “fig.” of Woman as the White Noise of History</a>
-                </li>
-                <li><a class="entry-b">Sonic Meditation “Ear Ly / Sex Change” </a></li>[<!-- Audio: february-phantomvoices-sonicmeditation.mp3] -->
-                <li><a class="entry-c">Food Poetics Club”</a></li>[<!-- Audio: later -->
-                <li><a class="entry-d">“Reflections on her Voice” visual score by Hannah Sakai”</a></li>[<!-- Audio: later -->
 
-                </ul>
-            </div>
+        </div>
 
         </main>
 
